5badff1e90b5583740c04086b0ac40ac5941c4a6
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-09 21:02:08 +0800

    Instrument lambda after handler is defined to fix error [200~partially initialized module 'app' has no attribute 'handler'~

--- app/app.py
+++ app/app.py
@@ -11,18 +11,19 @@ import json
 trace.set_tracer_provider(TracerProvider(resource=Resource.create({SERVICE_NAME: "local-lambda"})))
 otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4318/v1/traces")
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
-
 tracer = trace.get_tracer(__name__)
 
 
+# Define the handler *before* instrumentation
 def handler(event, context):
     print("Received event: ", event)
     try:
-        # Parse event here if needed
-        event_data = json.loads(event)
-        print("Parsed event data: ", event_data)
-        print(event_data)  # Log the parsed event data
+        event_data = event if not isinstance(event, str) else json.loads(event)
     except json.JSONDecodeError as e:
         print(f"Error parsing event: {e}")
         return {"statusCode": 400, "body": "Invalid input"}
     return {"statusCode": 200, "body": "ok"}
+
+
+# Instrument Lambda after handler is defined
+AwsLambdaInstrumentor().instrument()
